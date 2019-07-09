e544f4644190590bcfc413f6ea614debce9c590a
Abel David <abel.david@ignitarium.com>
2019-07-09 10:23:34 +0530

    commit

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,7 +2,7 @@ sudo docker run -it --net=host \
                 --env="DISPLAY" \
                 --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
                 --volume="$HOME/Workspace/Trails/z_docker_share:/root/home/host_share:rw" \
-                --name=gst_tt_2_gui \
+                --name=gst_tt_3_gui \
                 abelvdavid/my_ubuntu:git_only \
                 /bin/bash
 
