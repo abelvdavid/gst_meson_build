3f1480c809e4d97d2183255e364cc82707032e80
Abel David <abel.david@ignitarium.com>
2019-07-23 10:22:39 +0530

    init

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,7 +2,7 @@ sudo docker run -it --net=host \
                 --env="DISPLAY" \
                 --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
                 --volume="$HOME/Workspace/Trails/z_docker_share:/root/home/host_share:rw" \
-                --name=gst_tt_3_gui \
-                abelvdavid/my_ubuntu:git_only \
+                --name=jul_15_gst \
+                abelvdavid/my_ubuntu:gstreamer-1.8_MB \
                 /bin/bash
 
